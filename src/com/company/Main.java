8342a3c039d1d1d07fd45d06073c4ba0017c59f5
cmnguyen <cmnguyen@axon.com>
2021-10-30 00:44:02 +0700

    BST algorithm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.company.searching.bst.BST;
+import com.company.searching.symbol_table.BinarySearchST;
 import com.company.sorting.*;
 import com.company.sorting.heapsort.HeapSort;
 import com.company.sorting.mergesort.MergeSort;
@@ -7,26 +9,80 @@ import com.company.sorting.mergesort.TopDownMergeSort;
 import com.company.sorting.quicksort.QuickSort;
 import com.company.sorting.quicksort.QuickSort3Way;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
 
     public static void main(String[] args) {
-        Integer[] array = new Integer[] {2, 118, 10, 16, -2, 2, 100, 60, 70, 45, 17, 450, 18, 8};
-        QuickSort<Integer> quickSort = new QuickSort3Way<>();
-        quickSort.sort(array, Integer::compareTo);
-        System.out.println("Quick sort result: " + Arrays.toString(array));
+//        Integer[] array = new Integer[] {2, 118, 10, 16, -2, 2, 100, 60, 70, 45, 17, 450, 18, 8};
+//        QuickSort<Integer> quickSort = new QuickSort3Way<>();
+//        quickSort.sort(array, Integer::compareTo);
+//        System.out.println("Quick sort result: " + Arrays.toString(array));
+//
+//        MergeSort<Integer> mergeSort = new TopDownMergeSort<>();
+//        mergeSort.sort(array, (o1, o2) -> {
+//            if (o1.equals(o2)) return 0;
+//            else if (o1 > o2) return -1;
+//            else return 1;
+//        });
+//        System.out.println("BottomUpMergeSort decrease result: " + Arrays.toString(array));
+//
+//        Sort<Integer> heapSort = new HeapSort<>();
+//        heapSort.sort(array, Integer::compareTo);
+//        System.out.println("HeapSort result: " + Arrays.toString(array));
 
-        MergeSort<Integer> mergeSort = new TopDownMergeSort<>();
-        mergeSort.sort(array, (o1, o2) -> {
-            if (o1.equals(o2)) return 0;
-            else if (o1 > o2) return -1;
-            else return 1;
+//        BST<Character, Integer> bst = new BST<>();
+//        bst.put('S', 1);
+//        bst.put('H', 19);
+//        bst.put('U', 10);
+//        bst.put('E', 100);
+//        bst.put('T', 0);
+//        bst.put('Z', 15);
+//        bst.put('A', 17);
+//        bst.put('F', 18);
+//        bst.put('W', 25);
+//        bst.put('M', 27);
+//
+//        System.out.println("min: " + bst.min() + "\n");
+//        System.out.println("max: " + bst.max() + "\n");
+//        System.out.println("bst " + bst.toString());
+//
+//        System.out.println("lo and hi");
+//        Iterable<Character> keys1 = bst.keys('E', 'M');
+//        keys1.forEach(System.out::println);
+//
+//        bst.deleteMin();
+//        System.out.println("bst " + bst.toString());
+//        bst.delete('W');
+//        System.out.println("bst " + bst.toString());
+//
+//        System.out.println("bst floor of N: " + bst.floor('N'));
+
+        test();
+
+
+    }
+
+    private static void test() {
+        List<CompletableFuture<?>> futures = new ArrayList<>();
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            throw new IllegalArgumentException("my exception");
         });
-        System.out.println("BottomUpMergeSort decrease result: " + Arrays.toString(array));
+        CompletableFuture<String> completableFuture1 = CompletableFuture.supplyAsync(() -> "Test 1");
+        CompletableFuture<String> completableFuture2 = CompletableFuture.supplyAsync(() -> "Test 2");
+        CompletableFuture<String> completableFuture3 = CompletableFuture.supplyAsync(() -> "Test 3");
+        futures.add(completableFuture);
+        futures.add(completableFuture1);
+        futures.add(completableFuture2);
+        futures.add(completableFuture3);
+        CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(
+            futures.stream().map(it -> it.exceptionally(throwable -> null)).toArray(CompletableFuture[]::new));
+        combinedFuture.thenRun(() -> System.out.println("completed")).join();
 
-        Sort<Integer> heapSort = new HeapSort<>();
-        heapSort.sort(array, Integer::compareTo);
-        System.out.println("HeapSort result: " + Arrays.toString(array));
     }
 }
+
+
---
+++ src/com/company/searching/symbol_table/SequentialSearchST.java
@@ -0,0 +1,33 @@
+package com.company.searching.symbol_table;
+
+public class SequentialSearchST<Key, Value> {
+  private Node first;
+  private class Node {
+    Key key;
+    Value value;
+    Node next;
+    public Node(Key key, Value value, Node next) {
+      this.key = key;
+      this.value = value;
+      this.next = next;
+    }
+
+    public Value get(Key key) {
+      for (Node node = first; node != null; node = node.next) {
+        if (key.equals(node.key)) return node.value;
+      }
+
+      return null;
+    }
+
+    public void put(Key key, Value value) {
+      for (Node node = first; node != null; node = node.next) {
+        if (key.equals(node.key)) {
+          node.value = value;
+          return;
+        }
+      }
+      first = new Node(key, value, first);
+    }
+  }
+}
